ae3dc86a094921a437ffee72720aa14276d97d45
SeasonFox <coopoopoo@hotmail.com>
2017-01-06 15:24:09 +0800

    start dequeue

--- Source/Core/Containers/XCDequeue.h
+++ Source/Core/Containers/XCDequeue.h
@@ -3,15 +3,48 @@
 
 #include "../XCBasic.h"
 #include "../XCMemory.h"
+#include "../XCIterators.h"
 
 namespace XC
 {
-	template <typename T, typename TAllocator = DefaultAllocator<T> >
+	template <typename T, xsize TBufferSize = 0, typename TAllocator = DefaultAllocator<T> >
 	class Dequeue
 	{
 	public:
-		
-	}	
+		template <typename TReference, typename TPointer>
+		class Iterator
+		{
+		public:
+			typedef Iterator<TReference, TPointer> Self;
+			typedef Iterator<const T &, const T *> ConstantIterator;
+			typedef Iterator<T &, T *> Iterator;
+
+			// Iterator traits :
+			typedef RandomAccessIteratorTag IteratorCategory;
+			typedef T ValueType;
+			typedef TPointer Pointer;
+			typedef TReference Reference;
+			typedef xsize SizeType;
+			typedef xpointerdifference DifferenceType;
+
+			typedef T * * MapPointer; 
+
+			T * mCurrent;
+			T * mFirst;
+			T * mLast;
+			T * * mNode;
+		};
+	
+	public:
+		typedef T ValueType;
+		typedef T * Pointer;
+
+		typedef T * * MapPointer;
+
+	protected:
+		T * * mMapPointer;
+		xsize mMapSize;
+	};	
 }
 
 #endif // XCDEQUEUE_H
